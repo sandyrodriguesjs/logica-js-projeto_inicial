344bbe12ec2f995569db5b300eeac811c1a7e078
sandyrodriguesjs <sandyrodriguesdev@gmail.com>
2024-03-02 17:33:34 -0300

    definindo a variavel para gerar numeros aleatorios

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 alert("Boas vindas ao Jogo Número Secreto!");
-let numeroSecreto = 1;
+let numeroMaximo = 2000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo
+ + 1);
 console.log(numeroSecreto);
-let numeroEscolhido = prompt("Escolha um número entre 1 e 100");
+let numeroEscolhido;
+let tentativas = 1;
 
-if(numeroSecreto == numeroEscolhido){
-    alert(`Você acertou o Número Secreto ${numeroSecreto}!`);
-}else{
-    if(numeroEscolhido>numeroSecreto){
-        alert(`O Número Secreto é menor que ${numeroEscolhido}`);
-    }else{
-        alert(`O Número Secreto é maior que ${numeroEscolhido}`);
+while (numeroEscolhido != numeroSecreto) {
+    numeroEscolhido = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+    if (numeroEscolhido == numeroSecreto) {
+        break;
+    } else {
+        if (numeroEscolhido > numeroSecreto) {
+            alert(`O Número Secreto é menor que ${numeroEscolhido}`);
+        } else {
+            alert(`O Número Secreto é maior que ${numeroEscolhido}`);
+        }
+            tentativas++;
     }
-   
 }
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você acertou o Número Secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
